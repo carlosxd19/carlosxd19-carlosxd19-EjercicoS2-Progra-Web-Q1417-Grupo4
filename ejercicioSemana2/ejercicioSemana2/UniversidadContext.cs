7de5998c6e9450c645ec06fd33755679d6739e1d
carlos <161674271+carlosxd19@users.noreply.github.com>
2025-08-01 21:42:50 -0600

    feat: agregando el motodo 3
    Método 3: BuscarEstudiantePorEmail

--- ejercicioSemana2/ejercicioSemana2/UniversidadContext.cs
+++ ejercicioSemana2/ejercicioSemana2/UniversidadContext.cs
@@ -9,6 +9,28 @@ public class UniversidadContext
     public List<Estudiante> Estudiantes { get; set; } = new List<Estudiante>();
     public List<Profesor> Profesores { get; set; } = new List<Profesor>();
     public List<Cursos> Cursos { get; set; } = new List<Cursos>();
+
+    // Método 1: Agregar profesor
+    public void AgregarProfesor(Profesor profesor)
+    {
+        profesor.Id = Profesores.Count + 1;
+        Profesores.Add(profesor);
+    }
+
+    // Método 2: Agregar curso
+    public void AgregarCurso(Curso curso)
+    {
+        curso.Id = Cursos.Count + 1;
+        Cursos.Add(curso);
+    }
+    // Método 3: Buscar estudiante por email (insensible a mayúsculas)
+    public Estudiante BuscarEstudiantePorEmail(string email)
+    {
+        return Estudiantes
+            .FirstOrDefault(e => e.Email.Equals(email, StringComparison.OrdinalIgnoreCase));
+    }
+
+    // Método 3: Agregar estudiante
     public void agregar(Estudiante estudiante)
     {
         estudiante.Id = Estudiantes.Count + 1; // Auto-incremento ID
